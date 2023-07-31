1997fb8bbcbbfad17dc59ac48d7821384ef19d9f
Faizan <faizan.ahmed@empglabs.com>
2023-07-31 17:06:00 +0500

    Push To Talk functionality enabled + Changes in UI

--- interface/src/TranscribeOutput.js
+++ interface/src/TranscribeOutput.js
@@ -2,19 +2,22 @@ import React from "react";
 import withStyles from "@material-ui/core/styles/withStyles";
 import Typography from "@material-ui/core/Typography";
 
-
 const useStyles = () => ({
   root: {
-    maxWidth: '800px',
-    display: 'flex'
+    maxWidth: "800px",
+    display: "flex",
   },
   outputText: {
-    marginLeft: '8px',
-    color: '#ef395a',
-  }
+    marginLeft: "8px",
+    color: "#ef395a",
+  },
 });
 
-const TranscribeOutput = ({classes, transcribedText, interimTranscribedText}) => {
+const TranscribeOutput = ({
+  classes,
+  transcribedText,
+  interimTranscribedText,
+}) => {
   if (transcribedText.length === 0 && interimTranscribedText.length === 0) {
     return <Typography variant="body1">...</Typography>;
   }
@@ -22,9 +25,11 @@ const TranscribeOutput = ({classes, transcribedText, interimTranscribedText}) =>
   return (
     <div className={classes.root}>
       <Typography variant="body1">{transcribedText}</Typography>
-      <Typography className={classes.outputText} variant="body1">{interimTranscribedText}</Typography>
+      <Typography className={classes.outputText} variant="body1">
+        {interimTranscribedText}
+      </Typography>
     </div>
-  )
-}
+  );
+};
 
 export default withStyles(useStyles)(TranscribeOutput);
