d81bd96be729a3b665ac521f32e069554d1ff239
Faizan <faizan.ahmed@empglabs.com>
2023-08-01 18:24:16 +0500

    Whisper Model Large Loaded Globally rather than loading each time

--- backend/app.py
+++ backend/app.py
@@ -23,9 +23,12 @@ CORS(app)
 # Global variables to store the loaded model and tokenizer
 loaded_model = None
 loaded_tokenizer = None
+whisper_model = None
 
 def load_model_and_tokenizer():
-    global loaded_model, loaded_tokenizer
+    global loaded_model, loaded_tokenizer, whisper_model
+
+    whisper_model = whisper.load_model("large")
 
     if loaded_model is None and loaded_tokenizer is None:
         # Load the model from the file
@@ -66,13 +69,6 @@ load_model_and_tokenizer()
 @app.route('/transcribe', methods=['POST'])
 def transcribe():
     if request.method == 'POST':
-        language = request.form['language']
-        model = request.form['model_size']
-
-        # there are no english models for large
-        if model != 'large' and language == 'english':
-            model = model + '.en'
-        audio_model = whisper.load_model(model)
 
         temp_dir = tempfile.mkdtemp()
         save_path = os.path.join(temp_dir, 'temp.wav')
@@ -82,10 +78,7 @@ def transcribe():
         wav_file = request.files['audio_data']
         wav_file.save(save_path)
 
-        if language == 'english':
-            result = audio_model.transcribe(save_path, language='english')
-        else:
-            result = audio_model.transcribe(save_path)
+        result = whisper_model.transcribe(save_path, language='english')
         
         if result and result['text'] != '':
             print(result)
