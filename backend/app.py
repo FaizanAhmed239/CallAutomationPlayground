3ee877b095a117937fa4b5f6ac18c23ac26616d1
Faizan <faizan.ahmed@empglabs.com>
2023-08-08 17:38:31 +0500

    LFS Config + BERT modification

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,10 @@
 import pandas as pd
+import keras
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.models import Model
 from tensorflow.keras.models import load_model
+from tensorflow.keras.layers import Input
 import os
 import pickle
 import numpy as np
@@ -14,6 +16,7 @@ from flask import request
 from flask_cors import CORS
 from flask import jsonify
 import whisper
+from transformers import TFBertForSequenceClassification, BertTokenizer
 
 
 app = flask.Flask(__name__)
@@ -33,37 +36,62 @@ def load_model_and_tokenizer():
 
     if loaded_model is None and loaded_tokenizer is None:
         # Load the model from the file
-        loaded_model = load_model(
-            './my_model_2023-07-26_accuracy_0.941_loss_0.314_10.h5')
-        # Load the saved tokenizer
-        with open('./tokenizer_2023-07-26_accuracy_0.941_loss_0.314_10.pkl', 'rb') as f:
-            loaded_tokenizer = pickle.load(f)
+        # loaded_model = load_model(
+        #     './my_model_2023-07-26_accuracy_0.941_loss_0.314_10.h5')
+        # # Load the saved tokenizer
+        # with open('./tokenizer_2023-07-26_accuracy_0.941_loss_0.314_10.pkl', 'rb') as f:
+        #     loaded_tokenizer = pickle.load(f)
+        # Load the fine-tuned model and tokenizer
+        model_dir = "model_details/"
+        loaded_model = TFBertForSequenceClassification.from_pretrained(
+            model_dir)
+        # loaded_model = keras.models.load_model("tf_model.h5")
+        loaded_tokenizer = BertTokenizer.from_pretrained(model_dir)
 
 
 def predict_label(input_string, tokenizer, model):
 
-    # Tokenize the new input string and convert it into a sequence of indices
-    new_input_tokens = tokenizer.texts_to_sequences([input_string])
+    new_input_tokens = tokenizer.encode(input_string, add_special_tokens=True)
 
-    # Find the max_sequence_length from the new input tokens (assuming new_input_tokens is your list of input sequences)
-    # max_sequence_length = max(len(seq) for seq in new_input_tokens)
-    max_sequence_length = 10
+    # Find the max_sequence_length from the new input tokens
+    max_sequence_length = len(new_input_tokens)
 
     # Pad the sequence to match the same length as the sequences used during training
     new_input_sequence = pad_sequences(
-        new_input_tokens, maxlen=max_sequence_length, padding='post')
-
-    # Add an extra dimension to the input data (samples, timesteps, features)
-    new_input_sequence = new_input_sequence.reshape(-1, max_sequence_length, 1)
+        [new_input_tokens], maxlen=max_sequence_length, padding='post')
 
     # Make predictions using the trained model
     predicted_labels = model.predict(new_input_sequence)
 
     # Convert the predicted one-hot encoded label back to the original ID
-    predicted_id = int(np.argmax(predicted_labels, axis=-1)[0])
+    # predicted_id = int(np.argmax(predicted_labels[loaded_model], axis=-1)[0])
+    predicted_id = int(np.argmax(predicted_labels, axis=-1))
 
-    print(f"Predicted ID for '{input_string}': {predicted_id}\n")
+    print(f"Predicted ID for '{input_string}': {predicted_id}")
     return predicted_id
+    # # Tokenize the new input string and convert it into a sequence of indices
+    # new_input_tokens = tokenizer.texts_to_sequences([input_string])
+
+    # # Find the max_sequence_length from the new input tokens (assuming new_input_tokens is your list of input sequences)
+    # # max_sequence_length = max(len(seq) for seq in new_input_tokens)
+    # max_sequence_length = 10
+
+    # # Pad the sequence to match the same length as the sequences used during training
+    # new_input_sequence = pad_sequences(
+    #     new_input_tokens, maxlen=max_sequence_length, padding='post')
+
+    # # Add an extra dimension to the input data (samples, timesteps, features)
+    # new_input_sequence = new_input_sequence.reshape(-1, max_sequence_length, 1)
+
+    # # Make predictions using the trained model
+    # predicted_labels = model.predict(new_input_sequence)
+
+    # # Convert the predicted one-hot encoded label back to the original ID
+    # predicted_id = int(np.argmax(predicted_labels, axis=-1)[0])
+
+    # print(f"Predicted ID for '{input_string}': {predicted_id}\n")
+    # return predicted_id
+    # Tokenize the new input string and convert it into a sequence of indices
 
 
 # Call the function to load the model and tokenizer when the script starts
@@ -87,10 +115,10 @@ def transcribe():
             processed_text = punctuation_remover(result['text'])
             processed_text = modify_consecutive_words(processed_text)
             print(result)
-            print("Processed Text:" + processed_text)
+            # print("Processed Text:" + processed_text)
 
-            predicted_label = int(predict_label(
-                processed_text, loaded_tokenizer, loaded_model))
+            predicted_label = predict_label(
+                processed_text, loaded_tokenizer, loaded_model)
             print(predicted_label)
 
             return jsonify({'text': result['text'], 'predicted_id': predicted_label})
